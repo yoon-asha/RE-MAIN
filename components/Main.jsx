2a0169491512167dcf2fc418c2a184039fc4f9c9
yoon-asha <yoon.asha97@gmail.com>
2022-08-26 23:41:52 +0900

    0826-main 작업중

---
+++ components/Main.jsx
@@ -0,0 +1,39 @@
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
+import Logo from '../public/remain.png'
+
+const Main = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          width: { xs: "100vw", sm: "80vw" },
+          height: "70vh",
+          overflow: "hidden",
+          position: "relative",
+        }}
+      >
+        <Image
+          src="https://cdn.pixabay.com/photo/2019/06/22/08/10/leaves-4291098_1280.jpg"
+          layout="fill"
+          objectFit="cover"
+        />
+      </Box>
+
+      <Box sx={{
+          bgcolor: '#fff',
+          width: { xs: "100vw", sm: "80vw", md: '50vw' },
+          height: '300px',
+          position: 'absolute',
+          top: '60%',
+          pt: 1,
+          pl: 5, pr:5
+        }}>
+          <Typography>LOGO</Typography>
+          <Typography>일회용컵 회수 플랫폼 리:메인</Typography>
+        </Box>
+    </>
+  );
+};
+
+export default Main;
---
+++ components/Nav.jsx
@@ -0,0 +1,70 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  CssBaseline,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { styled, alpha } from "@mui/material/styles";
+import SearchIcon from "@mui/icons-material/Search";
+// import { Search } from "@mui/icons-material";
+import Image from "next/image";
+import Logo from "../public/remain.png";
+
+const Nav = () => {
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.black, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.black, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "200px",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  }));
+  return (
+    <>
+      {/* <CssBaseline /> */}
+      <AppBar position="sticky" sx={{ bgcolor: "#fff" }}>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Box sx={{ position: "relative", 
+          top: -40, height: 40}}>
+            <Image
+              src={Logo}
+              width={160}
+              // height={45}
+              // objectFit='none'
+              objectFit="contain"
+            />
+          </Box>
+
+          {/* <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ 
+              // display: { xs: 'none', sm: 'block' }, 
+              color:'#246551' }}>Logo</Typography> */}
+
+          <Search sx={{ color: "#246551" }}>
+            {/* <SearchIconWrapper> */}
+            <SearchIcon />
+            {/* </SearchIconWrapper> */}
+          </Search>
+
+          <Button variant="outlined" color="success">
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Nav;
